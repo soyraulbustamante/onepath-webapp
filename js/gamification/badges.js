58332a23f030c026445b4c457f8d2dd4f7b5c69a
juan333-fd <jjchipocom@gmail.com>
2025-11-02 15:44:29 -0500

    feat(user/gamification): Agrega imagenes,html y css de gamificacion

---
+++ js/gamification/badges.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".reward-card .btn-primary");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const reward = btn.parentElement.querySelector("h3").textContent;
+      alert(`🎉 Has canjeado: ${reward}`);
+    });
+  });
+});
